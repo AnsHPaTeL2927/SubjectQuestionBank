5e399d778d7fc9b7b40cb7212ce55da3b28b8112
AnsHPaTeL2927 <116540143+AnsHPaTeL2927@users.noreply.github.com>
2024-11-26 11:58:39 +0530

    cors origin changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const cors = require("cors")
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     methods: "GET, POST, HEAD, PUT, DELETE, PATCH",
     Credential: true
 }
